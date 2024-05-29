36bfbfac5d26fc6cf9e5a27ff557d0c39f08df08
seonghyunban <seonghyun.ban@pm.me>
2024-05-28 20:59:53 -0400

    created constructors

--- src/Tree.java
+++ src/Tree.java
@@ -3,4 +3,31 @@ import java.util.ArrayList;
 public class Tree<T> {
     private T root;
     private ArrayList<Tree<T>> subtrees;
+
+    // Constructors for empty trees
+    public Tree(){
+
+        this.root = null;
+        this.subtrees = new ArrayList<>();
+
+    }
+
+    // Constructor for trees with no children
+    public Tree(T root){
+
+        this.root = root;
+        this.subtrees = new ArrayList<>();
+
+    }
+
+    // Constructor for trees with children
+    public Tree(T root, ArrayList<Tree<T>> subtrees) {
+
+        this.root = root;
+        this.subtrees = new ArrayList<>(subtrees);
+    }
+
+
+
+
 }
